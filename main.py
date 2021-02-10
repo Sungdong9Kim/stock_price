b3245c2b582c43e7ca3d7a153b7139c8a445c6fa
Sungdong9Kim <sungdong9.kim@gmail.com>
2021-02-10 21:53:43 +0900

    feat : add function for other solutions

--- main.py
+++ main.py
@@ -20,5 +20,36 @@ def solution(prices):
         answer.append(no_price_down_time)
     return answer
 
+
+def correct_solution(prices):
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
+    return answer
+
+from collections import deque
+def correct_solution2(prices):
+    answer = []
+    prices = deque(prices)
+    while prices:
+        compared_prices = prices.popleft()
+
+        count = 0
+        for i in prices:
+            if compared_prices > i:
+                count += 1
+                break
+            count += 1
+        answer.append(count)
+
+    return answer
+
 prices = [5, 6, 3, 5, 2]
 print(f'{solution(prices)}')
+print(f'{correct_solution(prices)}')
+print(f'{correct_solution2(prices)}')
